771a17a345223de11731ff4f4d5d54a3496850ef
Agashi7 <113578642+Agashi7@users.noreply.github.com>
2025-03-27 02:05:22 +0530

    Update Project.jsx

--- src/app/Component/Project.jsx
+++ src/app/Component/Project.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useRef } from "react";
-import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
+import Projectcard from "./Projectcard";
 import { motion, useInView } from "framer-motion";
 
 const projectsData = [
