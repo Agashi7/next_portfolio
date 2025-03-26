d2a3dff36c7f6c765d75424acbec4131f336c43e
Agashi7 <113578642+Agashi7@users.noreply.github.com>
2025-03-27 01:59:55 +0530

    Update route.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req) {
   try {
     // Check if method is POST
     if (req.method !== "POST") {
